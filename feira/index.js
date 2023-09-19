a43c5abe5368217c0a3dad6b7a2c11c27d263b23
Gustavo Thyerris <132522484+GusThyerris@users.noreply.github.com>
2023-09-19 18:41:55 -0300

    Add files via upload

---
+++ feira/index.js
@@ -0,0 +1,18 @@
+
+setTimeout(
+    function() {
+        let sec1 = document.getElementById('sec_1').style.animation = "open-height 3s forwards ease-in-out"
+
+
+    }, 3500
+)
+
+setTimeout(
+    function() {
+        console.log('alo')
+        let div1 = document.getElementById('div_1').style.animation = "open-opacity 2s forwards ease-in-out"
+
+
+    }, 3500
+)
+
